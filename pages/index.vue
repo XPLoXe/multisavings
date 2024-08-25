efdbf121084ab6f2e9af4c11606ffb343200ffc8
Alejandro <aleb2800@gmail.com>
2024-08-25 12:47:12 +0200

    Title change

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,9 @@
 
     <!-- Title and Subtitle -->
     <div class="flex flex-col justify-center items-center text-center">
-      <h1 class="text-2xl md:text-3xl pt-6 text-white mt-10">Saving Project
+      <h1 class="text-2xl md:text-3xl pt-6 text-white mt-10">
+        <SvgIcon name="trending-up" alt="Trending Up" :width="30" :height="30" class="pt-2 md:w-9 md:h-9" />
+        Multisavings
         <SvgIcon name="coffee-alt" alt="Coffee Icon" :width="30" :height="30" class="pt-2 md:w-9 md:h-9" />
       </h1>
       <h3 class="text-white text-lg md:text-xl pt-4 md:pt-6">Manage your savings across multiple accounts</h3>
